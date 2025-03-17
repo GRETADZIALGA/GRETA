440798c734072f3f09d7170960f4a70826aea7da
GRETADZIALGA <GRETADZIALGA@GMAIL.COM>
2025-03-17 22:32:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const projects = document.querySelectorAll(".project-card");
+gsap.registerPlugin(ScrollTrigger);
 
-    document.addEventListener("scroll", () => {
-        let scrollY = window.scrollY;
-
-        projects.forEach((project, index) => {
-            let projectOffset = project.offsetTop - window.innerHeight / 1.5;
-
-            if (scrollY > projectOffset) {
-                project.style.opacity = "1";
-                project.style.transform = "scale(1)";
-            } else {
-                project.style.opacity = "0.3";
-                project.style.transform = "scale(0.8)";
-            }
-        });
+gsap.utils.toArray('.project').forEach((section) => {
+    gsap.from(section, {
+        opacity: 0,
+        y: 50,
+        scrollTrigger: {
+            trigger: section,
+            start: 'top 80%',
+            end: 'top 30%',
+            scrub: true,
+        }
     });
 });
