ad0f224f8b0755820c2b3ed6784540d33e16d766
GRETADZIALGA <GRETADZIALGA@GMAIL.COM>
2025-03-18 14:41:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ document.addEventListener("DOMContentLoaded", () => {
         let scrollY = window.scrollY;
 
         projects.forEach((project, index) => {
-            let projectOffset = window.innerHeight * (index + 1) * 0.8; // Staggered effect
+            let projectOffset = window.innerHeight * (index + 1) * 0.7; // Staggered effect
 
             if (scrollY > projectOffset) {
                 project.style.opacity = "1";
-                project.style.transform = `translateX(${window.innerWidth}px)`;
+                project.style.transform = `translateX(${(index % 2 === 0 ? "" : "-")}100vw)`;
             } else {
                 project.style.opacity = "0";
-                project.style.transform = `translateX(-100%)`;
+                project.style.transform = `translateX(${(index % 2 === 0 ? "-100vw" : "100vw")})`;
             }
         });
     });
